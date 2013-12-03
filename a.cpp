55d02fabe005b5a0973eed424a391627df2ad603
Josue Mora <josue@inno-live.com>
2013-12-03 02:42:48 -0800

    Initial commit

---
+++ a.cpp
@@ -0,0 +1,176 @@
+/*
+	CPSC 351 - Fall 2012
+	Assignment #3
+	Chapter 7: Banker's Algorithm
+	Description: Multithreaded program. 
+				 The banker will grant a request only if it leaves the system in a safe state. 
+				 A request that leaves the system in an unsafe state will be denied. 
+	Group Members: Harry Mora, Robert Rivas, Jennifer Peterson
+	Date: 12/03/13
+*/
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+// Class Banker definition
+class Banker{
+	private:
+		vector<int> seq;
+		int Need [5][3];
+		int Alocation [5][3];
+		int Max [5][3];
+		int Av [5][3];
+		int np, nr, i, x, j, k, a;
+	public:
+		void GetVector()
+		{
+		    cout << "Enter the allocation matrix." << endl;
+		    for (i = 0; i < 5; i++)
+		    {
+		    	for (j = 0; j < 3; j++)
+		        {
+		            cin >> x;
+		            Alocation[i][j] = x;
+		        }
+		    }
+		 
+		    cout << "Enter the max matrix." << endl;
+		    for (i = 0; i < 5; i++)
+		    {
+		        for (j = 0; j < 3; j++)
+		        {
+		            cin >> x;
+		            Max[i][j] = x;
+		            Need[i][j] = Max[i][j] - Alocation[i][j];
+		        }
+		    }
+		    cout << "Need matrix." << endl;
+		    for (i = 0; i < 5; i++)
+		    {
+		        for (j = 0; j < 3; j++)
+		        {
+		            cout << Need[i][j] << " ";
+		        }
+		        cout << endl;
+		    }
+		    cout << "Enter first entry of availability matrix." << endl;
+		    for (j = 0; j < 2; j++)
+		    {
+		        cin >> x;
+		        Av[0][j] = x;
+		    }	
+		}
+		void Request()
+		{
+			int P;
+			cout<<"Customer number 0-5"<<" "<<"Enter the request number "<<endl;
+			cin >>P;
+			switch (P)
+		    {
+		        case 1 : {
+		        	cout<<"You have entered customer number 1"<<
+		        			 " "<<"Please enter request number"<<endl;
+		        		int x,i;
+		        		int req[3];
+		        		cin>>x;
+		        		for (int j = 0; j < 2; j++)
+		            {
+		                cin >> x;
+		                Av[0][j] = x;
+		        		
+		        		}
+		        		if((req[3]<=Need[5][3])&&(req[3]<=Av[5][3]))
+		        			 for (i = 0; i < 5; i++)
+		            {
+		                for (int j = 0; j < 3; j++)
+		                {
+		                    
+		                    Av[i][j] = Av[i][j] - req[i];
+		        			Need[i][j]=Need[i][j]-req[i];
+		        			Alocation[i][j]=Alocation[i][j]+req[i];
+		                }
+		            }
+		        		else
+		        			cout<<"error"<<endl;
+		        		 };
+		        break;
+		        case 2 : cout << "Bravo";
+		        break;
+		        case 3 : cout << "Charlie";
+		        break;
+		        case 4 : cout << "Delta";
+		        break;
+		        case 5 : cout << "Echo";
+		        break;
+		    }
+		}
+		void FindPath()
+		{
+			bool flag[5];
+		    bool test = true;
+		    for (i = 0; i < 5; i++)
+		        flag[i] = false;
+		    int cnt = 0;
+		    while (cnt == 0)
+		    {
+		    for (i = 0; i < 5; i++)
+		    {
+		          if (flag[i] == false)
+		          {
+		              for (j = 0; j < 3; j++)
+		              {
+		                  if ((Need[i][j] > Av[i][j]))
+		                     test = false;
+		                  if ((Need[i][j] <= Av[i][j]))
+		                     test = true;
+		              }
+		              if (test == true) 
+		              {
+		                     seq.push_back(i);
+		                     for (k = 0; k < 3; k++)
+		                     {
+		                         Av[i][k] = Av[i][k] + Alocation[i][k];
+		                     }
+		                      flag[i] = true;
+		              } 
+		               
+		              } 
+		    } 
+		    for (a = 0; a < 5; a++)
+		    {
+		        if (flag[a] == false)
+		        {
+		           cnt = 0;
+		           break;
+		        }
+		        else
+		            cnt++;
+		    } 
+		   
+		    } 
+		   
+		    cout << "A possible sequence:" << endl;
+		    for (i = 0; i < 5; i++)
+		        cout << "P" << seq[i] << " -> ";
+		    cout << endl;
+		}
+};
+// end of class Banker
+int main(){
+	// create Banker Object
+	Banker A;
+	// class methods calls
+	A.GetVector();
+	//A.Request();
+	//A.FindPath();
+	return(0);
+}
+
+/*
+	I/O:
+
+
+*/
